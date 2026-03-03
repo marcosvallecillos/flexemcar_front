837e1fa6312100c417882161334d5c0113567464
Marcos Vallecillos <mavaus@floridauniversitaria.es>
2026-03-03 12:04:04 +0100

    Mostrando diferentes header dependiendo del rol q tiene, añadiendo estilos y animaciones a la web

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './views/home/home';
+import { ModalLoginComponent } from './components/modal-login/modal-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'login', component: ModalLoginComponent },
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,88 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Usuario } from './models/user.interface';
+import { ApiService } from './services/api-service.service';
+import { Header } from "./components/header/header";
+import { HeaderUser } from './components/header-user/header-user';
+import { HeaderAdmin } from './components/header-admin/header-admin';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header,HeaderUser,HeaderAdmin],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('myapp');
+   isUserRegistered: string | null = null;
+  isUser: boolean = false; 
+  isLoggedIn: boolean = false;
+  mostrarHeader: boolean = false;
+  usuario: Usuario | null = null;
+  isAdmin:boolean = false;
+    constructor(private router: Router, private apiService: ApiService) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (this.router.url !== '/home') {
+          window.scrollTo(0, 0);
+        }
+        this.mostrarHeader = this.router.url !== '/index' ; 
+        
+
+
+      }
+    });
+  }
+
+  
+  ngOnInit() {
+    this.actualizarEstadoUsuario();
+    
+    window.addEventListener("storage", () => {
+      this.actualizarEstadoUsuario();
+    });
+
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+    
+    this.router.events.subscribe(() => {
+      this.mostrarHeader = this.router.url !== '/index' && this.router.url !== '/policy-cookies' && this.router.url !== '/privacy-policy'; 
+    });
+    console.log('Mostrando header:', this.mostrarHeader);
+
+  }
+  
+  private actualizarEstadoUsuario() {
+    const userType = localStorage.getItem('userType');
+    const userData = localStorage.getItem('userData');
+    
+    this.isUserRegistered = userType || null;
+    this.isUser = userType === 'usuario';
+    this.isAdmin = userType === 'admin';
+    this.isLoggedIn = !!userData;
+
+    if (userData) {
+      try {
+        this.usuario = JSON.parse(userData);
+        
+      } catch (e) {
+        this.usuario = null;
+      }
+    } else {
+      this.usuario = null;
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('userData');
+    localStorage.removeItem('userType');
+    this.isUserRegistered = null;
+    this.isUser = false;
+    this.isLoggedIn = false;
+    this.usuario = null;
+    
+    window.dispatchEvent(new Event("storage"));
+    window.location.reload();
+  }
 }
