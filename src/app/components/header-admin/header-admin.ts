078fc85fb69187be8e1cf0877c8f90fa4ab2cf3a
Marcos Vallecillos <mavaus@floridauniversitaria.es>
2026-03-20 19:07:37 +0100

    feat: Mejorando codigo

--- src/app/components/header-admin/header-admin.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header-admin',
-  imports: [],
-  templateUrl: './header-admin.html',
-  styleUrl: './header-admin.css',
-})
-export class HeaderAdmin {}
